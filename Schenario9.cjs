9318b110ae6c984c00eb98e3868094117267ba56
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-09-13 13:37:23 +0530

    Completed Schenario 9

--- Schenario9.cjs
+++ Schenario9.cjs
@@ -5,3 +5,22 @@
 // if the cached data exists and is not older than cacheDuration (in milliseconds),
 //  it should return the cached data. Otherwise, 
 //  it should make a new API call and update the cache.
+
+const url='https://jsonplaceholder.typicode.com/users';
+//Solution
+
+const getUsersWithCache=async(cacheUserDuration)=>{
+    const cacheData=localStorage.getItem('cached_users');
+    const cacheTimestamp=localStorage.getItem('cache_timestamp');
+    const currentTime=Date.now();
+    if(cacheData && cacheTimestamp && (currentTime-cacheTimestamp)<cacheUserDuration){
+        return JSON.parse(cacheData);
+    }
+    else{
+        const fetchData=await fetch(url);
+        const userData=await fetchData.json();
+        localStorage.setItem('cached_users',JSON.stringify(userData));
+        localStorage.setItem('cache_timestamp',currentTime.toString());
+        return userData;
+    }
+}
