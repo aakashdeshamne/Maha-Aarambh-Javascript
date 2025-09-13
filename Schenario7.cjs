39ed4aa4fa50893ea7815bf8d28aa5f761d1b942
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-09-13 13:07:25 +0530

    Initiated Schenario 7

---
+++ Schenario7.cjs
@@ -0,0 +1,6 @@
+// 7. Storing Data in Local Storage
+// Write a function saveProductsToLocalStorage(products) that takes a products array, 
+// converts it to a JSON string,
+// and stores it in the browser's localStorage under the key user_products.
+
+//Answer
