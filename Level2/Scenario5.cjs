0113eb958a6315e448a9ea6e4bd425bbbf4b487e
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-09-19 00:37:27 +0530

    started with scenario5

---
+++ Level2/Scenario5.cjs
@@ -0,0 +1,9 @@
+// 5. Data Aggregation
+
+// Scenario: You have an array of sales transaction objects. 
+// You need to calculate the total sales amount for each product category.
+// Task: Write a function calculateCategorySales(transactions) 
+// that takes an array of transaction objects 
+// (e.g., [{ category: 'Electronics', amount: 1200 }, { category: 'Books', amount: 45 }, ...]) 
+// and returns an object where keys are the category names and values are the total sales
+//  for that category.
