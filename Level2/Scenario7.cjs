26b76f7f74f06f8314d4896292d6a2a65c5eeb99
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-09-20 16:08:05 +0530

    completed scenario 7

--- Level2/Scenario7.cjs
+++ Level2/Scenario7.cjs
@@ -9,3 +9,41 @@
 // If it's a cache miss (returns undefined), 
 // it should "fetch" the user from a simulated database function (that also returns a promise) 
 // and then set the result in the cache before returning the user.
+
+const model=require('../models/UserModel');
+
+const asyncCache={
+    store:{},
+    set(key,value){
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                this.store[key]=value;
+                resolve();
+            },50);       
+        })
+    },
+    get(key){
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve(this.store[key]);
+            },50);
+        })
+    }
+}
+
+const getUserFromDB = async (userId) => {
+    const user = await model.findOne(userId);
+    if (!user) throw new Error('User not found');
+    return user;
+};
+
+const getUserWithCache=async(userId)=>{
+    let user=await asyncCache.get(userId);
+    if(!user){
+        user=await getUserFromDB(userId);
+        if(!user) throw new Error('User not found');
+        await asyncCache.set(userId,user);
+        return user;
+    }
+    return user;
+}
